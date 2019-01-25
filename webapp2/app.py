8e1534d8031e0af8ea23c12e7be84d9b5f88067c
Jamiel Sheikh <i@jamiel.net>
2019-01-25 11:10:08 -0500

    day 5

--- webapp2/app.py
+++ webapp2/app.py
@@ -2,27 +2,17 @@
 
 from flask import Flask
 from flask import request
-
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '''
-    <!DOCTYPE html>
-    <html>
-        <head><title>Form Website</title></head>
-    <body>
-        <form action="/submitgame">
-        <p>How old are you?</p><br />
-        <input type="text" name="Number">
-        <input type="submit">
-        </form>
-    </body>
-    </html>
-    '''
-@app.route('/submitgame')
+    motd = 'Have a great one!'
+    return render_template('index.html',xyz=motd)
+
+@app.route('/submitgame',methods=['POST'])
 def submitgame():
-    int_num = request.args.get('Number')
+    int_num = request.form['Number']
     new_ans = int(int_num)**2
     return str(new_ans)
 
