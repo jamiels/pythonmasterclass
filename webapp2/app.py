029750210671cf50bbe3398add4d971aaf3150ee
Jamiel Sheikh <i@jamiel.net>
2019-01-25 15:07:48 -0500

    day 5

--- webapp2/app.py
+++ webapp2/app.py
@@ -3,17 +3,48 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import mysql.connector as mc
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    motd = 'Have a great one!'
-    return render_template('index.html',xyz=motd)
+    products = get_products()
+    return render_template('index.html',products=products)
 
-@app.route('/submitgame',methods=['POST'])
-def submitgame():
-    int_num = request.form['Number']
-    new_ans = int(int_num)**2
-    return str(new_ans)
+@app.route('/submitOrder',methods=['POST'])
+def submit_order():
+    quantity = request.form['quantity']
+    product = request.form['product']
+    insert_order(quantity,product)
+    return 'Order received!'
 
-    
+@app.route('/orders')
+def display_orders():
+    connection = get_connection()
+    result = connection.cmd_query("select * from orders")
+    rows = connection.get_rows()
+    connection.close()
+    return str(rows[0])
+
+
+def insert_order(qty,product_id):
+    connection = get_connection()
+    sql = 'INSERT INTO ORDERS(quantity,product_id) values('+qty+','+product_id+')'
+    print(sql)
+    connection.cmd_query(sql)
+    connection.commit()
+    connection.close()
+
+def get_products():
+    connection = get_connection()
+    result = connection.cmd_query("select * from products")
+    rows = connection.get_rows()
+    connection.close()
+    return rows[0]
+
+def get_connection():
+    return mc.connect(user='root', password='jamiel',
+                              host='127.0.0.1', database='candystore',
+                              auth_plugin='mysql_native_password')
+
+
